741dedaf1143bdf8601c2a841c24f283e2a4ab98
monahidalgo <monaego@live.com>
2022-01-25 16:58:11 -0700

    Conversion test.

--- src/main/java/edu/cnm/deepdive/Converter.java
+++ src/main/java/edu/cnm/deepdive/Converter.java
@@ -1,20 +1,37 @@
 package edu.cnm.deepdive;
 
+/**
+ * Implement a converter from the Celsius to Fahrenheit temperature scales. Double-precison
+ * calculations are used.
+ */
 public class Converter {
 
+  public static final double CELSIUS_TO_FAHRENHEIT_SCALE_FACTOR = 1.8;
+  public static final int CELSIUS_TO_FAHRENHEIT_INTERCEPT = 32;
+  .ERROR.FORMAT = "%s not recognized as a number.%n";
+
+  /**
+   * Converts the specified temperature (expressed in degrees Celsius, to the same temperature
+   * expressed in degrees Fahrenheit.
+   *
+   * @param c Celsius to be converted.
+   * @return Computed Fahrenheit temperature.
+   */
  public static double convertC2F(double c) {
-   return 1.8 * c + 32;
+   return CELSIUS_TO_FAHRENHEIT_SCALE_FACTOR * c + CELSIUS_TO_FAHRENHEIT_INTERCEPT;
  }
 
+  /**
+   * Iterates command-line arguments and attempts to convert each to a double, and then convert.
+   * @param args Command-line arguments zero or more Fahrenheit temperature values.
+   */
  public static void main(String [] args) {
    for (String arg : args) {
      try {
        double celsius = Double.parseDouble(arg);
-       System.out.printf("%.2f degrees Celsius = %.2f degrees Fahrenheit.%n", celsius, convertC2F(celsius));
+       System.out.printf CONVERSION_DISPLAY_FORMAT, celsius, convertC2F(celsius));
      } catch (NumberFormatException e) {
-       System.out.printf("%s not recognized as a number.%n", arg);
-     }
-   }
+       System.out.printf   }
  }
 
 }
--- src/test/java/edu/cnm/deepdive/ConverterTest.java
+++ src/test/java/edu/cnm/deepdive/ConverterTest.java
@@ -6,10 +6,27 @@ import org.junit.jupiter.api.Test;
 
 class ConverterTest {
 
+  static final double TOLERANCE = 0.00000001;
+
+  static final double[] celsiusValues = {
+      -40,
+      0,
+      100
+  };
+
+  static final double[] fahrenheitValues = {
+      -40,
+      32,
+      212
+  };
+
   @Test
   void convertC2F() {
-    assertEquals(212, Converter.convertC2F(100));
-    assertEquals(32, Converter.convertC2F(0));
-    assertEquals(-40, Converter.convertC2F(-40));
+    for (int i = 0; i < celsiusValues.length; i++) {
+      double celsius = celsiusValues[i];
+      double fahrenheit = fahrenheitValues[i];
+      assertEquals(fahrenheit, Converter.convertC2F(celsius), TOLERANCE);
+    }
   }
+
 }
