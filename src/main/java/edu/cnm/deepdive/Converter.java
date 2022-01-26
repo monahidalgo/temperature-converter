91bf937fc31c4c6cfed92ad6aab3a5db87c0b45f
monahidalgo <monaego@live.com>
2022-01-25 21:31:10 -0700

    Conversion test.

--- src/main/java/edu/cnm/deepdive/Converter.java
+++ src/main/java/edu/cnm/deepdive/Converter.java
@@ -8,7 +8,7 @@ public class Converter {
 
   public static final double CELSIUS_TO_FAHRENHEIT_SCALE_FACTOR = 1.8;
   public static final int CELSIUS_TO_FAHRENHEIT_INTERCEPT = 32;
-  .ERROR.FORMAT = "%s not recognized as a number.%n";
+  private static final String CONVERSION_DISPLAY_FORMAT ="%s not recognized as a number.%n";
 
   /**
    * Converts the specified temperature (expressed in degrees Celsius, to the same temperature
@@ -17,21 +17,25 @@ public class Converter {
    * @param c Celsius to be converted.
    * @return Computed Fahrenheit temperature.
    */
- public static double convertC2F(double c) {
-   return CELSIUS_TO_FAHRENHEIT_SCALE_FACTOR * c + CELSIUS_TO_FAHRENHEIT_INTERCEPT;
- }
+  public static double convertC2F(double c) {
+    return CELSIUS_TO_FAHRENHEIT_SCALE_FACTOR * c + CELSIUS_TO_FAHRENHEIT_INTERCEPT;
+  }
 
   /**
    * Iterates command-line arguments and attempts to convert each to a double, and then convert.
+   *
    * @param args Command-line arguments zero or more Fahrenheit temperature values.
    */
- public static void main(String [] args) {
-   for (String arg : args) {
-     try {
-       double celsius = Double.parseDouble(arg);
-       System.out.printf CONVERSION_DISPLAY_FORMAT, celsius, convertC2F(celsius));
-     } catch (NumberFormatException e) {
-       System.out.printf   }
- }
+  public static void main(String[] args) {
+    for (String arg : args) {
+      try {
+        double celsius = Double.parseDouble(arg);
+        System.out.printf (CONVERSION_DISPLAY_FORMAT, celsius, convertC2F (celsius));
+      } catch (NumberFormatException e) {
+        System.out.printf(PARSE_ERROR_FORMAT, arg);
+      }
+    }
+
+  }
 
 }
